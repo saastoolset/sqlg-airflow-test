d5dc9f498f019301280c8a748e49187781a1c48c
jesse <web.jesse@gmail.com>
2022-07-17 03:10:18 +0800

    dags testcase 1

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
 
 rem docker build --rm  -t jessewei/sqlg-airflow:latest . > build.log
 rem docker build -f Dockerfile.docker-airflow --rm  -t saastoolset/sqlg-airflow:latest .
-
 rem docker build -f Dockerfile.2.3 --rm  -t saastoolset/sqlg-airflow:2.3 .
 
-docker build --rm  -t saastoolset/sqlg-airflow:latest .
+docker build --rm  -t saastoolset/sqlg-airflow-test:latest .
 
 
 
